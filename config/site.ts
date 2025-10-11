9afdea66abc2e07617a82c5e3368d303f9b734ff
oyasumi-ryo <yanano1228@gmail.com>
2025-10-11 23:50:10 +0900

    ホーム画面、ヘッダのスタイリング

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Post Writer",
+  description: "",
+  url: "https://localhost:3000",
+  ogImage: "",
+  links: {
+    x: "https://twitter.com",
+    github: "https://github.com/oyasumi-ryo"
+  }
+};
